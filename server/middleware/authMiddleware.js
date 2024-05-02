45503f80445f68220e38e80ca32d1796c75d9981
Ashwin <ashwinkv.akv@gmail.com>
2024-05-02 18:43:00 +0530

    update log

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -3,6 +3,7 @@ import jwt from "jsonwebtoken";
 export  function isAuthenticated(req, res, next) {
   if (req.user) {
     const token = req.cookies.jwt;
+    console.log("token in host",token);
     const data = jwt.verify(token, process.env.REFRESH_TOKEN_SECRET);
     req.user.tokenData=data
     return next();
