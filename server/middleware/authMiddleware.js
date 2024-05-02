095cea6992f711b2afb108063133bc4192416251
Ashwin <ashwinkv.akv@gmail.com>
2024-05-02 18:46:29 +0530

    update log

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,12 +1,12 @@
 const CLIENT_URL = process.env.CLIENT_URL;
 import jwt from "jsonwebtoken";
-export  function isAuthenticated(req, res, next) {
-  if (req.user) {
-    const token = req.cookies.jwt;
-    console.log("token in host",token);
-    const data = jwt.verify(token, process.env.REFRESH_TOKEN_SECRET);
-    req.user.tokenData=data
-    return next();
+export function isAuthenticated(req, res, next) {
+  const token = req.cookies.jwt;
+  console.log("token in host", token);
+  const data = jwt.verify(token, process.env.REFRESH_TOKEN_SECRET);
+  req.user.tokenData = data;
+  if (!token) {
+    return res.status(401).send();
   }
-  res.status(401).send(); 
+  return next();
 }
