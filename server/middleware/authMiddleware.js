b492751f913dabe71a8f7dc852101ed9914b6402
Ashwin <ashwinkv.akv@gmail.com>
2024-05-24 21:59:45 +0530

    FIx jwt bug

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -2,10 +2,10 @@ const CLIENT_URL = process.env.CLIENT_URL;
 import jwt from "jsonwebtoken";
 export function isAuthenticated(req, res, next) {
   const token = req.cookies.jwt;
-  const data = jwt.verify(token, process.env.REFRESH_TOKEN_SECRET);
-  req.tokenData = data;
   if (!token) {
     return res.status(401).send();
   }
+  const data = jwt.verify(token, process.env.REFRESH_TOKEN_SECRET);
+  req.tokenData = data;
   return next();
 }
